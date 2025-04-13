22f073332abe694e299236b316d5608b6266ac30
leo <leo233.huang@gmail.com>
2025-04-13 22:29:32 +0800

    update vdev

--- package/kernel_module/rs/vdev/vdev.rs
+++ package/kernel_module/rs/vdev/vdev.rs
@@ -1,28 +1,93 @@
 //! Virtual Device Module
-use kernel::prelude::*;
+use core::pin::Pin;
+use kernel::{
+    c_str,
+    device::Device,
+    fs::File,
+    miscdevice::{MiscDevice, MiscDeviceOptions, MiscDeviceRegistration},
+    new_mutex,
+    prelude::*,
+    sync::Mutex,
+    types::ARef,
+};
+
+struct Inner {
+    value: i32,
+}
+
+#[pin_data(PinnedDrop)]
+struct VDev {
+    #[pin]
+    inner: Mutex<Inner>,
+    dev: ARef<Device>,
+}
+
+#[vtable]
+impl MiscDevice for VDev {
+    type Ptr = Pin<KBox<Self>>;
+
+    fn open(_file: &File, misc: &MiscDeviceRegistration<Self>) -> Result<Pin<KBox<Self>>> {
+        let dev = ARef::from(misc.device());
+        dev_info!(dev, "Opening Rust VDev --- called when User Open\n");
+        KBox::try_pin_init(
+            try_pin_init! {
+                VDev {
+                    inner <- new_mutex!( Inner {value: 0i32}),
+                dev:dev
+                }
+            },
+            GFP_KERNEL,
+        )
+    }
+}
+
+#[pinned_drop]
+impl PinnedDrop for VDev {
+    fn drop(self: Pin<&mut Self>) {
+        dev_info!(self.dev, "Exiting Rust VDev --- called when User Close\n");
+    }
+}
 
 module! {
-    type: VDev,
+    type: VDevModule,
     name: "vdev",
-    authors: ["Rust for Linux Contributors"],
+    authors: ["vdev"],
     description: "Rust VDev sample",
     license: "GPL",
 }
 
-struct VDev;
+//if `pin_data!` used only, we cannot impl `Drop` trait for it
+//#[pin_data]
+#[pin_data(PinnedDrop)]
+struct VDevModule {
+    #[pin]
+    _miscdev: MiscDeviceRegistration<VDev>,
+}
 
-impl kernel::Module for VDev {
-    fn init(_module: &'static ThisModule) -> Result<Self> {
+// called when insmod
+impl kernel::InPlaceModule for VDevModule {
+    fn init(_module: &'static ThisModule) -> impl PinInit<Self, Error> {
         // Print a banner to make sure our module is working
         pr_info!("------------------------\n");
-        pr_info!("Rust VDev (enter)\n");
+        pr_info!("Rust VDevModule (enter)\n");
         pr_info!("------------------------\n");
-        Ok(VDev)
+
+        // this will create /dev/vdev
+        let options = MiscDeviceOptions {
+            name: c_str!("vdev-miscdev"),
+        };
+        try_pin_init!(Self {
+            _miscdev <- MiscDeviceRegistration::register(options)
+        })
     }
 }
 
-impl Drop for VDev {
-    fn drop(&mut self) {
-        pr_info!("Rust VDev (exit)\n");
+// called when rmmod
+#[pinned_drop]
+impl PinnedDrop for VDevModule {
+    fn drop(self: Pin<&mut Self>) {
+        pr_info!("------------------------\n");
+        pr_info!("Rust VDevModule (exit)\n");
+        pr_info!("------------------------\n");
     }
 }
